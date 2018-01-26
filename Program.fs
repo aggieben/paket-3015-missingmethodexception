9e80120e4e7fc3b910f7aa7336e510acaeb9cc5e
Ben Collins <aggieben@gmail.com>
2018-01-26 14:31:36 -0600

    initial commit

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let server = Prometheus.Client.MetricServer.MetricServer(9090)
+    server.Start()
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
